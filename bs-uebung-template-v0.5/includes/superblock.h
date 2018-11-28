5dc21ba3eeffa36d7ecac1c2725249ab6cf14820
Michael Martel <codered.dev.germany@gmail.com>
2018-11-28 12:35:30 +0100

    Added superblock

---
+++ bs-uebung-template-v0.5/includes/superblock.h
@@ -0,0 +1,27 @@
+//
+//  superblock.h
+//  mkfs.myfs
+//
+//  Created by Michael Martel on 26.11.18.
+//
+#ifndef superblock_h
+#define superblock_h
+
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/stat.h>
+
+struct SuperBlock {
+    u_int32_t size;
+    char name[256];
+    u_int32_t dmap_block;
+    u_int32_t fat_block;
+    u_int32_t rootdir_block;
+
+    // Not sure if needed
+    u_int32_t freeBlockCount;
+    u_int32_t first_free_block;
+    time_t change_time;
+};
+
+#endif // superblock_h
